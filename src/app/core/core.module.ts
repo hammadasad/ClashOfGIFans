5f55463fee3f3a7cb8a04c0e3cc8d4fa8bf91bdf
Hammad Asad <hammad_asad@msn.com>
2018-04-03 17:00:50 -0400

    Connected Routes to Views

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from '@app/core/footer/footer.component';
 import { HeaderComponent } from '@app/core/header/header.component';
 
+import { RouterModule } from '@angular/router';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [FooterComponent, HeaderComponent],
   exports: [FooterComponent, HeaderComponent]
